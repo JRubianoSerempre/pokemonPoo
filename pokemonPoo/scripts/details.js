c31e1f157f58cf36fe8158350413e81ed21164e1
jhonRubiano <jrubiano@serempre.com>
2022-02-02 15:39:44 -0500

    feat:indent

--- pokemonPoo/scripts/details.js
+++ pokemonPoo/scripts/details.js
@@ -55,93 +55,93 @@ function renderPokemon({ id, name, base_experience, types, weight, sprites, heig
             pokemonInfo.setAttribute('style',` overflow-y:scroll
                                     `)
 
-      const container = document.createElement('div');
-            container.classList.add('pokemon')
-      
-      const title = document.createElement('h1');
-            title.textContent = `N.${('00'+id).slice(-3)}: ${name.charAt(0).toUpperCase()+ name.slice(1)}`;
-            title.classList.add('pokemon__title');
-      const img = document.createElement('img');
-            img.src = `${sprites.other["official-artwork"].front_default}`;
-            img.setAttribute('style',`
-                                    image-rendering: pixelated;
-                                    width:300px;
-            `)
-      const typesContainer = document.createElement('div')
-            typesContainer.classList.add('pokemon__types')
-            types.forEach(id => {
-            const typeContainer = document.createElement('div');
-                  typeContainer.textContent = `${id.type.name.toUpperCase()}`;
-                  typeContainer.setAttribute('style',`background: ${colors[id.type.name]};`)
-                  typesContainer.appendChild(typeContainer)
-                  })
-  //Obtaining info from pokemon
-      const infoUl = document.createElement('ul');
-            infoUl.classList.add('pokemon__info');
-            const pokemonXp = document.createElement('li');
-                  pokemonXp.innerHTML = `<b>XP</b>:\xa0\xa0\xa0 ${base_experience}`;
-            const pokemonWeight = document.createElement('li');
-                  pokemonWeight.innerHTML = `<b>Weight</b>:\xa0\xa0\xa0 ${weight / 10} kg`;
-            const pokemonHeight = document.createElement('li');
-                  pokemonHeight.innerHTML = `<b>Height</b>:\xa0\xa0\xa0 ${height / 10} m`;
-            const pokemonAbilities = document.createElement('li');
-                  pokemonAbilities.classList.add('pokemon__info-abilities');
-                  pokemonAbilities.textContent = "ABILITIES";
-                  const abilitiesTable = document.createElement('table');
-                        const tbody = document.createElement('tbody');
-                        abilities.forEach( id =>getAbilities(id.ability.name,tbody))
-
-                        abilitiesTable.appendChild(tbody)  
-                  pokemonAbilities.appendChild(abilitiesTable)
-            infoUl.appendChild(pokemonXp)
-            infoUl.appendChild(pokemonWeight)
-            infoUl.appendChild(pokemonHeight)
-            infoUl.appendChild(pokemonAbilities)
-
-///BASIC STATS FOR POKEMON
-      const basicStatsContainer = document.createElement('div');
-            basicStatsContainer.classList.add('pokemon__stats');
-            const statsTitle = document.createElement('h2');
-                  statsTitle.textContent =`Basic Stats`;
-            const statsTable = document.createElement('table');
-                  statsTable.classList.add('stats');
-
-                  const tbodyStats =  document.createElement('tbody');
-                        stats.forEach(id => {
-                        const trStats = document.createElement('tr');
-                              const tdStatName = document.createElement('td');
-                                    tdStatName.classList.add('stats__name')
-                                    tdStatName.textContent = `${id.stat.name.toUpperCase()}`;
-
-                              const tdStatValue = document.createElement('td');
-                                    tdStatValue.classList.add('stats__value')
-                                    const statBar = document.createElement('div');
-                                          statBar.classList.add('stats__bar');
-                                          const statBarFilled = document.createElement('div');
-                                                statBarFilled.classList.add('stats__bar-filled');
-                                                statBarFilled.textContent = `${id.base_stat}`;
-                                                statBarFilled.setAttribute('style',`width: ${id.base_stat >100 ?'100': id.base_stat}%;
-                                                                                    background: ${colors[types[0].type.name]};
-                                                                                    `)
-                                          statBar.appendChild(statBarFilled);
-                                    tdStatValue.appendChild(statBar)
-
-                              trStats.appendChild(tdStatName);
-                              trStats.appendChild(tdStatValue);
-                        tbodyStats.appendChild(trStats)
+            const container = document.createElement('div');
+                  container.classList.add('pokemon')
+            
+            const title = document.createElement('h1');
+                  title.textContent = `N.${('00'+id).slice(-3)}: ${name.charAt(0).toUpperCase()+ name.slice(1)}`;
+                  title.classList.add('pokemon__title');
+            const img = document.createElement('img');
+                  img.src = `${sprites.other["official-artwork"].front_default}`;
+                  img.setAttribute('style',`
+                                          image-rendering: pixelated;
+                                          width:300px;
+                  `)
+            const typesContainer = document.createElement('div')
+                  typesContainer.classList.add('pokemon__types')
+                  types.forEach(id => {
+                  const typeContainer = document.createElement('div');
+                        typeContainer.textContent = `${id.type.name.toUpperCase()}`;
+                        typeContainer.setAttribute('style',`background: ${colors[id.type.name]};`)
+                        typesContainer.appendChild(typeContainer)
                         })
-                  statsTable.appendChild(tbodyStats)
+      //Obtaining info from pokemon
+            const infoUl = document.createElement('ul');
+                  infoUl.classList.add('pokemon__info');
+                  const pokemonXp = document.createElement('li');
+                        pokemonXp.innerHTML = `<b>XP</b>:\xa0\xa0\xa0 ${base_experience}`;
+                  const pokemonWeight = document.createElement('li');
+                        pokemonWeight.innerHTML = `<b>Weight</b>:\xa0\xa0\xa0 ${weight / 10} kg`;
+                  const pokemonHeight = document.createElement('li');
+                        pokemonHeight.innerHTML = `<b>Height</b>:\xa0\xa0\xa0 ${height / 10} m`;
+                  const pokemonAbilities = document.createElement('li');
+                        pokemonAbilities.classList.add('pokemon__info-abilities');
+                        pokemonAbilities.textContent = "ABILITIES";
+                        const abilitiesTable = document.createElement('table');
+                              const tbody = document.createElement('tbody');
+                              abilities.forEach( id =>getAbilities(id.ability.name,tbody))
 
-            basicStatsContainer.appendChild(statsTitle)
-            basicStatsContainer.appendChild(statsTable)
+                              abilitiesTable.appendChild(tbody)  
+                        pokemonAbilities.appendChild(abilitiesTable)
+                  infoUl.appendChild(pokemonXp)
+                  infoUl.appendChild(pokemonWeight)
+                  infoUl.appendChild(pokemonHeight)
+                  infoUl.appendChild(pokemonAbilities)
+
+      ///BASIC STATS FOR POKEMON
+            const basicStatsContainer = document.createElement('div');
+                  basicStatsContainer.classList.add('pokemon__stats');
+                  const statsTitle = document.createElement('h2');
+                        statsTitle.textContent =`Basic Stats`;
+                  const statsTable = document.createElement('table');
+                        statsTable.classList.add('stats');
+
+                        const tbodyStats =  document.createElement('tbody');
+                              stats.forEach(id => {
+                              const trStats = document.createElement('tr');
+                                    const tdStatName = document.createElement('td');
+                                          tdStatName.classList.add('stats__name')
+                                          tdStatName.textContent = `${id.stat.name.toUpperCase()}`;
+
+                                    const tdStatValue = document.createElement('td');
+                                          tdStatValue.classList.add('stats__value')
+                                          const statBar = document.createElement('div');
+                                                statBar.classList.add('stats__bar');
+                                                const statBarFilled = document.createElement('div');
+                                                      statBarFilled.classList.add('stats__bar-filled');
+                                                      statBarFilled.textContent = `${id.base_stat}`;
+                                                      statBarFilled.setAttribute('style',`width: ${id.base_stat >100 ?'100': id.base_stat}%;
+                                                                                          background: ${colors[types[0].type.name]};
+                                                                                          `)
+                                                statBar.appendChild(statBarFilled);
+                                          tdStatValue.appendChild(statBar)
+
+                                    trStats.appendChild(tdStatName);
+                                    trStats.appendChild(tdStatValue);
+                              tbodyStats.appendChild(trStats)
+                              })
+                        statsTable.appendChild(tbodyStats)
+
+                  basicStatsContainer.appendChild(statsTitle)
+                  basicStatsContainer.appendChild(statsTable)
 
 
-      container.appendChild(title);
-      container.appendChild(img);
-      container.appendChild(typesContainer);
-      container.appendChild(infoUl);
-      container.appendChild(basicStatsContainer)
-      pokemonInfo.appendChild(container);
+            container.appendChild(title);
+            container.appendChild(img);
+            container.appendChild(typesContainer);
+            container.appendChild(infoUl);
+            container.appendChild(basicStatsContainer)
+            pokemonInfo.appendChild(container);
       _domManipulate.render(pokemonInfo, 'body')
       const body = document.querySelector('body')
             body.setAttribute('style',`background: linear-gradient(${colors[types[0].type.name]},rgb(200, 211, 196))`)
